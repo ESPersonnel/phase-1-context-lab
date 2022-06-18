5cf4af6ba3024ee2edf963dbe857a50d42fe1d78
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-18 22:00:53 +0300

    commit tree 3a7f80841c107d8541ff9ca7ed502d4cbfd7de39

--- index.js
+++ index.js
@@ -1,5 +1,85 @@
 /* Your Code Here */
 
+// // Solution 1
+let createEmployeeRecord = function(employee){
+    return {
+      firstName: employee[0],
+      familyName: employee[1],
+      title: employee[2],
+      payPerHour: employee[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    }
+  }
+  
+  // Solution 2
+  function createEmployeeRecords(arrayOfArrays){
+    return arrayOfArrays.map(createEmployeeRecord)
+  }
+  
+  // Solution 3
+  const getHour = function(dateTime){
+    return parseInt(dateTime.match(/\d{4}$/)[0]) // returns the last two digits
+  }
+  
+  // Solution 4
+  const getDate = function(dateTime){
+    return dateTime.match(/\d{4}-\d{2}-\d{2}/)[0] // returns the date
+  }
+  
+  // Solution 5
+  function createTimeInEvent(timeIn){
+    this.timeInEvents.push({
+      type: "TimeIn",
+      date: getDate(timeIn),
+      hour: getHour(timeIn)
+    })
+    return this
+  }
+  
+  // Solution 6
+  function createTimeOutEvent(timeOut){
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      date: getDate(timeOut),
+      hour: getHour(timeOut)
+    })
+    return this
+  }
+  
+  // Solution 7
+  function hoursWorkedOnDate(dateOne){
+    let timeIn = this.timeInEvents.find(function(event){
+      return event.date === dateOne
+    })
+    let timeOut = this.timeOutEvents.find(function(event){
+      return event.date === dateOne
+    })
+    let totalTime = (timeOut.hour - timeIn.hour) / 100
+    return totalTime
+  }
+  
+  // Solution 8
+  function wagesEarnedOnDate(dateOne){
+    let hours = hoursWorkedOnDate.call(this, dateOne)
+    return this.payPerHour * hours
+  }
+  
+  // Solution 9
+  function findEmployeeByFirstName(array, name){
+    return array.find(function(employee){
+      return employee.firstName === name
+    })
+  }
+  
+  // Solution 10
+  function calculatePayroll(employeeRecord){
+    return employeeRecord.reduce((total, employee) => {
+        return total + allWagesFor.call(employee)
+    }, 0)
+  }
+  
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
